6f8968c77d774419aeb08b89b6a98619b24938f0
MathewSachin <mathew.sachin@github.com>
2015-10-13 20:53:33 +0530

    Moved Combinatorics to namespace `MathParsing`

--- Combinatorics.cs
+++ Combinatorics.cs
@@ -1,36 +1,39 @@
-﻿public static class Combinatorics
+﻿namespace MathParsing
 {
-    public static double Factorial(int n)
+    public static class Combinatorics
     {
-        if (n < 0) return double.NaN;
-        else if (n == 0) return 1;
-        else
+        public static double Factorial(int n)
         {
-            double Product = 1;
-            for (int i = 1; i <= n; ++i) Product *= i;
-            return Product;
+            if (n < 0) return double.NaN;
+            else if (n == 0) return 1;
+            else
+            {
+                double Product = 1;
+                for (int i = 1; i <= n; ++i) Product *= i;
+                return Product;
+            }
         }
-    }
 
-    public static double C(int n, int r)
-    {
-        if (r < 0 | n < 0 | n < r) return double.NaN;
-        else if (n - r == 1 | r == 1) return n;
-        else if (n == r | r == 0) return 1;
-        else if (n - r > r) return (P(n, n - r) / Factorial(n - r));
-        else return (P(n, r) / Factorial(r));
-    }
-
-    public static double P(int n, int r)
-    {
-        if (r < 0 | n < 0 | n < r) return double.NaN;
-        else if (r == 0) return 1;
-        else if (n == r) return Factorial(n);
-        else
+        public static double C(int n, int r)
         {
-            double Product = 1;
-            for (int i = n - r + 1; i <= n; ++i) Product *= i;
-            return Product;
+            if (r < 0 | n < 0 | n < r) return double.NaN;
+            else if (n - r == 1 | r == 1) return n;
+            else if (n == r | r == 0) return 1;
+            else if (n - r > r) return (P(n, n - r) / Factorial(n - r));
+            else return (P(n, r) / Factorial(r));
+        }
+
+        public static double P(int n, int r)
+        {
+            if (r < 0 | n < 0 | n < r) return double.NaN;
+            else if (r == 0) return 1;
+            else if (n == r) return Factorial(n);
+            else
+            {
+                double Product = 1;
+                for (int i = n - r + 1; i <= n; ++i) Product *= i;
+                return Product;
+            }
         }
     }
 }
