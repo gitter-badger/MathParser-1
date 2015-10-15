a6c7a01e8dd00287e48efa58e25e34548bd44d3e
MathewSachin <mathew.sachin@github.com>
2015-10-15 16:35:05 +0530

    Internalized Operator Base Constructor

--- Tokens/Operators/Operator Base.cs
+++ Tokens/Operators/Operator Base.cs
@@ -2,7 +2,7 @@
 {
     public class Operator : Token
     {
-        public Operator(string Keyword, int Priority = 0) : base(Keyword, Priority) { }
+        internal Operator(string Keyword, int Priority = 0) : base(Keyword, Priority) { }
 
         public static readonly Operator LeftParenthesis = new Operator("("),
             RightParenthesis = new Operator(")");
