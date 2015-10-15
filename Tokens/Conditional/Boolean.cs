5a6a2c0104cea5e25cd422c65828fa0e13ca54ea
MathewSachin <mathew.sachin@github.com>
2015-10-15 11:20:29 +0530

    Boolean Tokens are on the Way

---
+++ Tokens/Conditional/Boolean.cs
@@ -0,0 +1,13 @@
+﻿namespace MathParsing
+{
+    class Boolean : Token
+    {
+        Boolean(bool Keyword) : base(Keyword.ToString(), 0) { }
+
+        public static readonly Boolean True = new Boolean(true),
+            False = new Boolean(false);
+
+        public static implicit operator double(Boolean B) { return B.Keyword.Contains("true") ? 1 : 0; }
+        public static implicit operator Boolean(double D) { return D == 0 ? False : True; }
+    }
+}
---
+++ Tokens/Conditional/BooleanOperator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MathParsing
+{
+    abstract class BooleanOperator : Operator
+    {
+        public BooleanOperator(string Keyword, int Priority) : base(Keyword, Priority) { }
+    }
+
+    class ComparisonOperator : BooleanOperator
+    {
+        Func<double, double, bool> Procedure;
+
+        public ComparisonOperator(string Keyword, int Priority, Func<double, double, bool> Procedure)
+            : base(Keyword, Priority)
+        {
+            this.Procedure = Procedure;
+        }
+
+        public bool Invoke(double Arg1, double Arg2) { return Procedure(Arg1, Arg2); }
+
+        public static readonly ComparisonOperator Equals = new ComparisonOperator("==", 0, (x, y) => x == y),
+            NotEquals = new ComparisonOperator("!=", 0, (x, y) => x != y),
+            GreaterThan = new ComparisonOperator(">", 0, (x, y) => x > y),
+            LessThan = new ComparisonOperator("<", 0, (x, y) => x < y);
+    }
+    
+    class BinaryBooleanOperator : BooleanOperator
+    {
+        Func<bool, bool, bool> Procedure;
+
+        public BinaryBooleanOperator(string Keyword, int Priority, Func<bool, bool, bool> Procedure)
+            : base(Keyword, Priority)
+        {
+            this.Procedure = Procedure;
+        }
+
+        public bool Invoke(bool Arg1, bool Arg2) { return Procedure(Arg1, Arg2); }
+
+        public static readonly BinaryBooleanOperator And = new BinaryBooleanOperator("&", 0, (x, y) => x && y),
+            Or = new BinaryBooleanOperator("|", 0, (x, y) => x || y);
+    }
+}
