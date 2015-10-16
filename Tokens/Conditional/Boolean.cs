96a0e0a18d5dad7f1331e9914aafaa0fe451ce46
MathewSachin <mathew.sachin@github.com>
2015-10-16 21:09:01 +0530

    Added Implicit Conversion b/w bool and Boolean

--- Tokens/Conditional/Boolean.cs
+++ Tokens/Conditional/Boolean.cs
@@ -7,6 +7,8 @@
         public static readonly Boolean True = new Boolean(true),
             False = new Boolean(false);
 
+        public static implicit operator bool(Boolean B) { return B.Keyword.Contains("true"); }
+        public static implicit operator Boolean(bool b) { return b ? True : False; }
         public static implicit operator double(Boolean B) { return B.Keyword.Contains("true") ? 1 : 0; }
         public static implicit operator Boolean(double D) { return D == 0 ? False : True; }
     }
